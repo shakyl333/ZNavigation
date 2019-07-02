e581db2614f0e871b55a9599f63537bd6e5c8c4e
shakyl333 <shakyl.mansuri@gmail.com>
2019-07-02 09:59:06 +0000

    navigation comepleted

--- webapp/controller/V_Source.controller.js
+++ webapp/controller/V_Source.controller.js
@@ -1,45 +1,31 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
-
 	return Controller.extend("ZNavigation.ZNavigation.controller.V_Source", {
-
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf ZNavigation.ZNavigation.view.V_Source
 		 */
-		onInit: function () {
+		onInit: function () {},
+		/**
+		 *@memberOf ZNavigation.ZNavigation.controller.V_Source
+		 */
+		GoToTarget1: function (oEvent) {
+			// Now Get the Router Info
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this); 
 
+			// Tell the Router to Navigate To Route_Tar_1
+			oRouter.navTo("Route_Tar_1", {});
 		},
-
 		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf ZNavigation.ZNavigation.view.V_Source
+		 *@memberOf ZNavigation.ZNavigation.controller.V_Source
 		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+		GoToTarget2: function (oEvent) {
+			// Now Get the Router Info
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf ZNavigation.ZNavigation.view.V_Source
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf ZNavigation.ZNavigation.view.V_Source
-		 */
-		//	onExit: function() {
-		//
-		//	}
-
+			// Tell the Router to Navigate To Route_Tar_2
+			oRouter.navTo("Route_Tar_2", {});
+		}
 	});
-
 });
--- webapp/controller/V_Target_2.controller.js
+++ webapp/controller/V_Target_2.controller.js
@@ -1,45 +1,38 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"], function (Controller,History) {
 	"use strict";
-
 	return Controller.extend("ZNavigation.ZNavigation.controller.V_Target_2", {
-
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf ZNavigation.ZNavigation.view.V_Target_2
 		 */
-		onInit: function () {
-
+		onInit: function () {},
+		/**
+		 *@memberOf ZNavigation.ZNavigation.controller.V_Target_2
+		 */
+		GoOneScreenBack: function (oEvent) {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+			// Go one screen back if you find a Hash
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			}
+			// If you do not find a correct Hash, go to the Source screen using default router;
+			else {
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.navTo("", true);
+			}
 		},
-
 		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf ZNavigation.ZNavigation.view.V_Target_2
+		 *@memberOf ZNavigation.ZNavigation.controller.V_Target_1
 		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
 		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf ZNavigation.ZNavigation.view.V_Target_2
+		 *@memberOf ZNavigation.ZNavigation.controller.V_Target_1
 		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf ZNavigation.ZNavigation.view.V_Target_2
-		 */
-		//	onExit: function() {
-		//
-		//	}
-
+		GotoSource: function (oEvent) {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			// Tell the Router to Navigate To Route_Tar_2
+			oRouter.navTo("Route_Source", {});
+		}
 	});
-
 });
